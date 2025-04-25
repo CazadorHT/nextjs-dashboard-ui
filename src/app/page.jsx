be631e6361a7b8530fe6bc013b85dce918b84e99
CazadorHT <117435698+CazadorHT@users.noreply.github.com>
2025-04-25 12:22:47 +0700

    first commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,8 @@ import Container from './components/Container'
 import Navbar from "./components/Navbar";
 import Vercel from '../../public/next.svg'
 import Footer from "./components/Footer";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
@@ -14,9 +16,9 @@ export default function Home() {
             <p>Become full-Stack Dev w Nextjs</p>
             <div className="flex justify-center my-10">
               <Image src={Vercel} width={300} height={0} alt="Vercel logo"/>
+              <Link href='/welcome'>Cilck Me Go To Main page</Link>
             </div>
           </div>
-          
       </Container>
       <Footer />
    </main>
